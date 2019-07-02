f021cca1ee1ed72b4885825530e11ea4c69fe4b6
neil <arifrahman@hotmail.com>
2019-07-01 20:27:10 -0400

    basic html and java running

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+//doctype : JavaScript - Trivia Game
+
+
+console.log ("testing for Trivia Game");
+
+$(document).ready(function() {
+//var isreturn_str = $('#first-question').data('return');
+//var isreturn_str = $(this).data('return');
+//console.log (isreturn_str);
+//console.log (typeof isreturn_str);
+
+
+//var isreturn = (isreturn_str === 'true');
+
+
+
+
+$("#first-question").on("click", function() {
+    var isreturn_str = $('#first-question').data('return');
+//var isreturn_str = $(this).data('return');
+console.log (isreturn_str);
+console.log (typeof isreturn_str);
+
+})
+
+})
